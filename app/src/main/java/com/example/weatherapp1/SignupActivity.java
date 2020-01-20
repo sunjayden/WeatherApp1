93755454ea761ccc2690181810bb05a25c981d35
Jayden <jayden_sun@outlook.com>
2020-01-20 14:28:25 -0500

    Add name to sign up screen

--- app/src/main/java/com/example/weatherapp1/SignupActivity.java
+++ app/src/main/java/com/example/weatherapp1/SignupActivity.java
@@ -10,7 +10,6 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,18 +18,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonRegister;
     private EditText editTextEmail;
     private EditText editTextPassword;
+    private EditText editTextName;
     private TextView textViewSignup;
 
     private ProgressDialog progressDialog;
 
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
 
 //    @Override
 //    public void onStart() {
@@ -50,6 +52,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button) findViewById(R.id.signupButton);
+        editTextName = (EditText) findViewById(R.id.signupName);
         editTextEmail = (EditText) findViewById(R.id.signupEmail);
         editTextPassword = (EditText) findViewById(R.id.signupPassword);
         textViewSignup = (TextView) findViewById(R.id.signupGoLogin);
@@ -59,9 +62,10 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void registerUser(){
+        final String name = editTextName.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
-//        System.out.println(email);
+        System.out.println(name);
 //        System.out.println(password);
         if (TextUtils.isEmpty(email)){
             Toast.makeText(this,"Please enter email", Toast.LENGTH_SHORT).show();
@@ -80,7 +84,13 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
-                            Toast.makeText(SignupActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this, "Registered Successfully. Please Sign In", Toast.LENGTH_SHORT).show();
+                            String currentUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                            mDatabase = FirebaseDatabase.getInstance().getReference();
+                            mDatabase.child("users").child(currentUser).setValue(name);
+
+                            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
                         }else {
                             FirebaseException e = (FirebaseException)task.getException();
                             Toast.makeText(SignupActivity.this, "Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
