eaad55c6a939eab9a918110bb1492d9f3b5dfec0
Jayden <jayden_sun@outlook.com>
2020-01-20 14:45:54 -0500

    Add name retrieval in main screen

--- app/src/main/java/com/example/weatherapp1/MainActivity.java
+++ app/src/main/java/com/example/weatherapp1/MainActivity.java
@@ -9,6 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -21,10 +29,11 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView cityName;
-    Button searchButton;
-    TextView result;
-    TextView info;
+    private TextView cityName;
+    private Button searchButton;
+    private TextView result;
+    private TextView info;
+    private TextView name;
 
     class Weather extends AsyncTask<String,Void,String> {
 
@@ -113,6 +122,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        name = findViewById(R.id.name);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        String uId = "mb7sZcpnxiT7Ka8MM1rXaJAVPCd2";
+        // Read from the database
+        myRef.child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                name.setText("Hi, " + value + "!");
+                Log.d("value", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("abc", "Failed to read value.", error.toException());
+            }
+        });
 
     }
 }
